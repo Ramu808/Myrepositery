6cd64f5c71fd0491590c867f5d925ff385e494f9
Ramu <ramaiahudauyagiri89@gmail.com>
2021-04-06 16:18:07 +0530

    message added

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import React,{Component} from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+export default class App extends Component{
+  render(){
+    return(
+      <View>
+        <Text>Hiii</Text>
+      </View>
+    );
+  }
+}
+
